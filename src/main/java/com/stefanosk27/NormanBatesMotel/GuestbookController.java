27e845a8f0c85d4c66b242f142e8f0d6fde204c5
Stefanos Kostaras <s.kostaras@JRT90.chq.jrtechnologies.com>
2018-02-08 12:26:05 +0200

    fixed redirect problem. Researching for "unable to load js" problem

--- src/main/java/com/stefanosk27/NormanBatesMotel/GuestbookController.java
+++ src/main/java/com/stefanosk27/NormanBatesMotel/GuestbookController.java
@@ -13,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.RedirectView;
 import com.stefanosk27.NormanBatesMotel.Guestbook;
 import com.stefanosk27.NormanBatesMotel.GuestbookService;
 
 
 @Controller
+@RequestMapping("/")
 public class GuestbookController {
 //	
 //    @GetMapping("/jsp")
@@ -59,36 +62,68 @@ public class GuestbookController {
 	}
 
 
-	@RequestMapping(value = "/addComment", method = RequestMethod.POST, headers = "Accept=application/json")
-//	@ResponseBody
-	public String addComment(@ModelAttribute("guestbook") Guestbook guestbook) {	
-		if(guestbook.getId()==0)
-		{
-			guestbookService.addComment(guestbook);
-		}
-		else
-		{	
-			guestbookService.updateComment(guestbook);
-		}
-		
-//		return "redirect:/getAllComments#guestbook";
-		return "redirect:/getAllComments";
-//		return "redirect:jsp/guestbookPage";
-//		return "http://localhost:8080/getAllComments";
-//		return "/getAllComments";
-	}
+//	@RequestMapping(value = "/addComment", method = RequestMethod.POST, headers = "Accept=application/json")
+////	@ResponseBody
+//	public String addComment(@ModelAttribute("guestbook") Guestbook guestbook) {	
+//		if(guestbook.getId()==0)
+//		{
+//			guestbookService.addComment(guestbook);
+//		}
+//		else
+//		{	
+//			guestbookService.updateComment(guestbook);
+//		}
+//		
+////		return "redirect:/getAllComments#guestbook";
+//		return "redirect:/getAllComments";
+////		return "redirect:jsp/guestbookPage";
+////		return "http://localhost:8080/getAllComments";
+////		return "/getAllComments";
+//	}
+	
+	   @RequestMapping(value = "/addComment", method = RequestMethod.POST, headers = "Accept=application/json")
+//	  @ResponseBody
+	    public RedirectView addComment(@ModelAttribute("guestbook") Guestbook guestbook) {    
+	        if(guestbook.getId()==0)
+	        {
+	            guestbookService.addComment(guestbook);
+	        }
+	        else
+	        {   
+	            guestbookService.updateComment(guestbook);
+	        }
+	        
+//	      return "redirect:/getAllComments#guestbook";
+//	        return "redirect:/getAllComments";
+//	      return "redirect:jsp/guestbookPage";
+//	      return "http://localhost:8080/getAllComments";
+//	      return "/getAllComments";
+	        return new RedirectView("/getAllComments");
+	    }
+	    
+	
+//    @GetMapping("/redirectWithRedirectView")
+//    public RedirectView redirectWithUsingRedirectView(RedirectAttributes attributes) {
+//        attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
+//        attributes.addAttribute("attribute", "redirectWithRedirectView");
+//        return new RedirectView("redirectedUrl");
+//    }
 
 	@RequestMapping(value = "/updateComment/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
 	public String updateComment(@PathVariable("id") int id,Model model) {
 		 model.addAttribute("guestbook", this.guestbookService.getComment(id));
 	        model.addAttribute("allComments", this.guestbookService.getAllComments());
 	        return "jsp/guestbookPage";
+//	        return new RedirectView("/addComment");
 	}
 
 	@RequestMapping(value = "/deleteComment/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
-	public String deleteComment(@PathVariable("id") int id) {
+	public RedirectView deleteComment(@PathVariable("id") int id) {
 		guestbookService.deleteComment(id);
-		 return "redirect:/getAllComments";
+		System.out.println("hello");
+		// return "redirect:/getAllComments";
+//		 return "getAllComments";
+		 return new RedirectView("/getAllComments");
 
 	}	
 }
